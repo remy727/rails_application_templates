0dfaf546f37e4764dd1a8f29bac44353ea8d5455
Remy Wang <remy.wang727@gmail.com>
2023-07-28 11:21:03 -0400

    Update Sidekiq template

--- sidekiq.rb
+++ sidekiq.rb
@@ -9,7 +9,12 @@ end
 gem "sidekiq"
 do_bundle
 
-github_raw_url = "https://raw.githubusercontent.com/remy727/rails_application_templates/main/sidekiq/config/initializers/sidekiq.rb"
-file_path = "config/initializers/sidekiq.rb"
+puts
+if yes?("Generate config/initializers/sidekiq.rb? [y/N]")
+  github_raw_url = "https://raw.githubusercontent.com/remy727/rails_application_templates/main/sidekiq/config/initializers/sidekiq.rb"
+	file_path = "config/initializers/sidekiq.rb"
 
-download_file(github_raw_url, file_path)
+	download_file(github_raw_url, file_path)
+end
+
+say("Sidekiq Successfully Installed", :blue)
