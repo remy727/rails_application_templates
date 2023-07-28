edd03b30756e9289eea76234e5fad9699c2f196d
Remy Wang <remy.wang727@gmail.com>
2023-07-28 12:00:00 -0400

    Update base template

--- base.rb
+++ base.rb
@@ -1,7 +1,11 @@
-if yes?("== Prepare database? [y/N] ==")
+if yes?("Prepare database? [y/N]")
 	rails_command "db:prepare"
 end
 
-if yes?("== Disable helper generator? [y/N] ==")
+if yes?("Disable helper generator? [y/N]")
 	environment "config.generators.helper = false"
+end
+
+if yes?("Disable helper generator? [y/N]")
+	run "bundle lock --add-platform x86_64-linux"
 end
